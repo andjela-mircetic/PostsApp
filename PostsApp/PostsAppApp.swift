ed7fbe85fbe20f904bfcdb78bd80d933cafb067d
andjela-mircetic <andjelamircetic55@gmail.com>
2025-06-12 01:29:19 +0200

    Initial Commit

---
+++ PostsApp/PostsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostsAppApp.swift
+//  PostsApp
+//
+//  Created by Andjela Mircetic on 12.6.25..
+//
+
+import SwiftUI
+
+@main
+struct PostsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
